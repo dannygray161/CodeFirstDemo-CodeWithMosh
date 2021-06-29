1b2f2b86db4656bdfa0cd4bfac24b971ac27d8b8
Daniel Gray <dieseldan14@gmail.com>
2021-06-29 18:26:36 -0500

    Add project files.

---
+++ PlutoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext()
+            : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
